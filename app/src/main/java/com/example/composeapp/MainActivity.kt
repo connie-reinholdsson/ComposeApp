05e25a8bb91c8c0bed2c2532180fe51ef4d660bb
connie-reinholdsson <connie.reinholdsson@gmail.com>
2020-06-07 18:12:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.tooling.preview.Preview
+import com.example.composeapp.ui.AppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppTheme {
+        Greeting("Android")
+    }
+}
