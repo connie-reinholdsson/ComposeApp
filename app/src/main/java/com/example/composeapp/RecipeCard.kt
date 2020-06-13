dd53c68b086208080b6c23d01c6e4aaee837fb5f
connie-reinholdsson <connie.reinholdsson@gmail.com>
2020-06-13 11:30:59 +0100

    1 All the ingredients in list under image.

--- app/src/main/java/com/example/composeapp/RecipeCard.kt
+++ app/src/main/java/com/example/composeapp/RecipeCard.kt
@@ -1,12 +1,26 @@
 package com.example.composeapp
 
 import androidx.compose.Composable
+import androidx.ui.core.Alignment
+import androidx.ui.foundation.Box
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.layout.Column
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 @Composable
 fun RecipeCard(recipe: Recipe) {
-    Text(recipe.title)
+    val image = imageResource(id = recipe.imageResource)
+    Column {
+        Image(asset = image)
+        Text(text = recipe.title)
+
+        for (ingredient in recipe.ingredients) {
+            Text(text = ingredient)
+        }
+    }
 }
 
 @Composable
