90540f6e36dbe0c031815a73461eea086ff12e5b
connie-reinholdsson <connie.reinholdsson@gmail.com>
2020-06-14 09:12:39 +0100

    1 Added padding to text and wrapped everything in another column.

--- app/src/main/java/com/example/composeapp/RecipeCard.kt
+++ app/src/main/java/com/example/composeapp/RecipeCard.kt
@@ -1,26 +1,35 @@
 package com.example.composeapp
 
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.Column
+import androidx.ui.layout.padding
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
 import androidx.ui.res.imageResource
+import androidx.ui.text.TextStyle
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 
 @Composable
 fun RecipeCard(recipe: Recipe) {
     Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
-        val image = imageResource(id = recipe.imageResource)
         Column {
-            Image(asset = image)
-            Text(text = recipe.title)
+            val image = imageResource(id = recipe.imageResource)
+            Column {
+                Image(asset = image)
+            }
 
-            for (ingredient in recipe.ingredients) {
-                Text(text = "• $ingredient")
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = recipe.title, style = TextStyle(fontSize = 24.sp))
+
+                for (ingredient in recipe.ingredients) {
+                    Text(text = "• $ingredient")
+                }
             }
         }
     }
